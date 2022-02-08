d2e8c8078dbade0178692ebaf1e3316ad550cd75
Matteo Pompei <datxizzan@gmail.com>
2022-02-08 22:39:27 +0100

    Sezione Telephone fatta

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <FirstLesson />
+    <Telephone />
   </main>
 </template>
 
 <script>
 import FirstLesson from "../sections/FirstLesson.vue";
+import Telephone from "../sections/Telephone.vue";
 
 export default {
   name: "Main",
   components: {
     FirstLesson,
+    Telephone,
   },
 };
 </script>
---
+++ src/components/sections/Telephone.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="left">
+        <div class="title">Start Learning to Drive Today</div>
+        <div class="subtitle">
+          Give us a call to schedule your first driving lesson
+        </div>
+      </div>
+      <div class="right">
+        <div class="phone"><i class="fa-solid fa-phone"></i> 1-800-555-555</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Telephone",
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  padding: 100px 0;
+  background: url("../../assets/img/horizontal-cta-background.jpg") center;
+  background-size: cover;
+  color: #fff;
+
+  .container {
+    align-items: center;
+
+    .title {
+      margin-bottom: 20px;
+      font-size: 2.7rem;
+      font-weight: bold;
+    }
+
+    .subtitle {
+      font-size: 1.4rem;
+      font-weight: bold;
+    }
+
+    .phone {
+      font-size: 2rem;
+
+      .fa-phone {
+        transform: translateX(-20px);
+      }
+    }
+  }
+}
+</style>
