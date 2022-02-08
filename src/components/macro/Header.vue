cf35f01a7ea6cb659301b9272971222b5607ae4b
Matteo Pompei <datxizzan@gmail.com>
2022-02-08 18:16:57 +0100

    Tutto l'header realizzato

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,11 +1,71 @@
 <template>
-  <header></header>
+  <header>
+    <div class="top-bar">
+      <div class="container">
+        <span>We have a 95% Successful Pass Rate!</span>
+        <span
+          ><i class="fa-solid fa-phone"></i> Give us a call to book your
+          tuition! 1-800-555-555</span
+        >
+      </div>
+    </div>
+    <section class="jumbo">
+      <Navbar />
+      <div class="container">
+        <div class="title">Drive with Avada</div>
+        <div class="subtitle">
+          We offer the finest driving tuition money can buy
+        </div>
+      </div>
+    </section>
+  </header>
 </template>
 
 <script>
+import Navbar from "../sections/Navbar.vue";
+
 export default {
   name: "Header",
+  components: {
+    Navbar,
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.top-bar {
+  padding: 15px 0;
+  background: #333333;
+
+  span {
+    font-size: 0.8rem;
+    color: #7799ad;
+
+    .fa-phone {
+      transform: translateX(-10px);
+    }
+  }
+}
+
+.jumbo {
+  position: relative;
+  padding: 20% 50px 10% 50px;
+  background: url("../../assets/img/homepage-hero-background.jpg");
+  background-size: cover;
+  color: #fff;
+
+  .container {
+    width: 60%;
+    justify-content: flex-start;
+    flex-direction: column;
+  }
+  .title {
+    font-size: 3.6rem;
+    font-weight: bold;
+  }
+
+  .subtitle {
+    font-size: 1.4rem;
+  }
+}
+</style>
---
+++ src/components/sections/Navbar.vue
@@ -0,0 +1,143 @@
+<template>
+  <section>
+    <div class="container">
+      <img
+        src="../../assets/img/avada-drivers-logo-1x.png"
+        alt="Avada Driving School"
+      />
+      <nav>
+        <ul>
+          <li v-for="(element, index) in menuArr" :key="index">
+            <a
+              href="#"
+              :class="{
+                active: element.isActive,
+                new: element.isNew,
+                booknow: element.lastItem,
+              }"
+              >{{ element.text }}</a
+            >
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      menuArr: [
+        {
+          text: "Home",
+          isActive: true,
+          isNew: false,
+        },
+        {
+          text: "About",
+          isActive: false,
+          isNew: false,
+        },
+        {
+          text: "Prices",
+          isActive: false,
+          isNew: false,
+        },
+        {
+          text: "Courses",
+          isActive: false,
+          isNew: true,
+        },
+        {
+          text: "Locations",
+          isActive: false,
+          isNew: false,
+        },
+        {
+          text: "Blog",
+          isActive: false,
+          isNew: false,
+        },
+        {
+          text: "Book now",
+          lastItem: true,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/vars.scss";
+
+section {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 30px 0;
+  background: rgba(51, 51, 51, 0.4);
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    ul {
+      list-style: none;
+
+      li {
+        display: inline-block;
+        margin: 0 20px;
+
+        a {
+          padding-bottom: 15px;
+          font-size: 0.9rem;
+          color: #fff;
+          text-decoration: none;
+          font-weight: bold;
+          text-transform: uppercase;
+          border-bottom: 2px solid transparent;
+          transition: 0.5s;
+
+          &:hover,
+          &.active {
+            color: $accent;
+            border-bottom: 2px solid $accent;
+          }
+
+          &.booknow {
+            background: $accent;
+            padding: 13px 30px;
+            border: none;
+            border-radius: 30px;
+
+            &:hover {
+              background: #64a64e;
+              color: inherit;
+              padding: 13px 30px;
+              border-bottom: inherit;
+            }
+          }
+
+          &.new::after {
+            margin-left: 10px;
+            padding: 2px 5px;
+            content: "New";
+            font-size: 0.7rem;
+            text-transform: uppercase;
+            background: $accent;
+            border-radius: 3px;
+          }
+
+          &.new:hover {
+            color: #fff;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
